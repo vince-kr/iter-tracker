5ebf1bc178286a4f52eb7afe0996de5121cf7f9a
Vince <vince@krijnsen.com>
2023-03-04 21:39:46 +0000

    Create Day object that can return date strings and sum the duration of StudySession objects it owns

--- agile.py
+++ agile.py
@@ -1,17 +1,12 @@
+from datetime import date
+
 class StudySession:
     def __init__(self, goal:str, start:str, end:str) -> None:
         """Instantiate a study session with goal, start time, and end time"""
-        self.goal = goal.capitalize()
-        self.duration = self._getDuration(start, end)
+        self.goal = goal
+        self.duration = self._calculateDuration(start, end)
 
-    def getGoalAndDuration(self) -> dict:
-        """Return a dict with the goal worked on and duration of the session"""
-        return {
-                "goal":self.goal,
-                "duration":self.duration
-                }
-
-    def _getDuration(self, starttime:str, endtime:str) -> int:
+    def _calculateDuration(self, starttime:str, endtime:str) -> int:
         """Helper method to convert string start and end times into minutes"""
         start_hr, start_min = self._formatTimeString(starttime)
         end_hr, end_min = self._formatTimeString(endtime)
@@ -22,3 +17,30 @@ class StudySession:
         if len(time) == 4:  # in case of missing leading 0 for hours between 0 and 10
             time = "0" + time
         return (int(time[:2]), int(time[3:]))
+
+
+class Day:
+    def __init__(self, datestring:str) -> None:
+        self.date = date.fromisoformat(
+                datestring[:4] + "-" + datestring[4:6] + "-" + datestring[6:]
+                )
+        self.study_sessions = []
+
+    def getDayOfWeek(self) -> str:
+        """Return full name of the weekday (e.g. 'Monday')"""
+        return self.date.strftime("%A")
+
+    def getDayAndMonth(self) -> str:
+        """Return day of the month and short month name (e.g. '4 Mar')"""
+        return self.date.strftime("%-d %b")
+
+    def generateSession(self, goal:str, start:str, end:str) -> None:
+        """Add a study session to the day's list of study sessions"""
+        self.study_sessions.append(StudySession(goal, start, end))
+
+    def getSessionTotals(self) -> dict:
+        """Sum the total minutes spent on learning goal and build goal this day"""
+        totals = { "build": 0, "learn": 0 }
+        for session in self.study_sessions:
+            totals[session.goal] += session.duration
+        return totals
--- test_agile.py
+++ test_agile.py
@@ -1,11 +1,34 @@
 import unittest
-from agile import StudySession
+from agile import StudySession, Day, Iteration
 
-class TestAgile(unittest.TestCase):
+class TestStudySession(unittest.TestCase):
 
     def testStudySessionReturnsGoalAndDuration(self):
-        ss = StudySession("build", "8:00", "9:00")
-        self.assertEqual(ss.getGoalAndDuration(), {
-            "goal":"Build",
-            "duration":60
+        ss = StudySession("build", "8:06", "9:18")
+        self.assertEqual(ss.goal, "build")
+        self.assertEqual(ss.duration, 72)
+
+
+class TestDay(unittest.TestCase):
+
+    def testDayReturnsTwoTypesOfDateString(self):
+        day = Day("20230304")
+        self.assertEqual(day.getDayOfWeek(), "Saturday")
+        self.assertEqual(day.getDayAndMonth(), "4 Mar")
+
+    def testDayReturnsSumOfItsSessions(self):
+        day = Day("20230304")
+        day.generateSession("build", "12:40", "13:30")
+        day.generateSession("build", "14:41", "15:16")
+        day.generateSession("learn", "20:12", "20:58")
+        day.generateSession("learn", "23:08", "23:47")
+        self.assertEqual(day.getSessionTotals(), {
+            "build":85,
+            "learn":85
             })
+
+
+class TestIteration(unittest.TestCase):
+
+    def TestIterationIsAwareOfItsOwnLength(self):
+        it = Iteration("20230304", 14)
