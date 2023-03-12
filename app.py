a6ab25be1c42fd73408c3887114ef1021bf4923b
Vince <vince@krijnsen.com>
2023-03-12 08:57:10 +0000

    Form to add study sessions works

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from agile import Agile
 from datetime import date
 
 app = Flask(__name__)
-progress_tracker = Agile()
+a = Agile()
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
     if request.method == "GET":
-        it = progress_tracker.current_iteration
-        it.todays_date = date.today()
-        return render_template("tracker_page", iteration = it)
+        iteration = a.current_iteration
+        iteration.todays_date = date.today()
+        return render_template("tracker_page", iteration = iteration)
     else: # it's POST
-        for key in request.form:
-            print(key, request.form[key])
+        date_of_session = date.fromisoformat(request.form["session_date"])
+        session_data = (
+                date_of_session,
+                request.form["goal_type"],
+                request.form["start_time"],
+                request.form["end_time"]
+                )
+        addStudySessionToCurrentIteration(a.current_iteration, session_data)
+        return redirect(url_for("index"))
 
 def addStudySessionToCurrentIteration(iteration:object, session_data:tuple) -> object:
-    iteration.generateSession(*session_data)
+    try:
+        iteration.generateSession(*session_data)
+    except AttributeError:
+        iteration.error_message = "ERROR: overlapping study sessions"
     return iteration
