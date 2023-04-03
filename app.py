2438793692440f3f07b86c4aa46e5d8b423437da
Vince <vince@krijnsen.com>
2023-04-03 22:18:00 +0100

    Reworking layout (and secretly some logic)

--- app.py
+++ app.py
@@ -1,22 +1,31 @@
 from flask import Flask, render_template, request, redirect, url_for
-from agile import Agile
+from agile import Iteration
 from datetime import date
 
 app = Flask(__name__)
-a = Agile()
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    iteration = a.current_iteration
+    # iteration = Iteration()
     if request.method == "GET":
         context = {
-            "todays_date": date.today(),
-            "counter": iteration.counter,
-            "start_to_end": iteration.start_to_end,
-            "weeks": iteration.weeks,
-            "study_sessions": iteration.study_sessions,
-            "goals": iteration.goals,
+            "learning": {
+                "description": "3hrs studying Fluent Python (at least finish ch.5, which is 30 more pages); 1hr practicing TCR.",
+                "time_spent": "0:00",
+                "time_target": "4:00",
+                "percentage_of_target": "0.00%",
+            },
+            "building": {
+                "description": "I want to implement showing the progress on an iteration in terms of percentage of the time goal versus how much of the iteration has passed. I also want to keep working on the note transposing problem. Say 3hrs for Iteration-Tracker and 1hr on music.",
+                "time_spent": "2:15",
+                "time_target": "4:00",
+                "percentage_of_target": "56.25%",
+            },
+            "weeks": [
+                list(range(7)),
+                list(range(7)),
+            ],
         }
         return render_template("tracker_page", **context)
     else:  # it's POST
