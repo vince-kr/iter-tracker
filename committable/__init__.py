8fe91579460ed2b52741c271abb822917d9600fa
Vince <vince@krijnsen.com>
2023-04-15 11:11:24 +0100

    Ensure that persistence directory exists

--- committable/__init__.py
+++ committable/__init__.py
@@ -1,3 +1,13 @@
 from . import iteration
+import os
+
+# Set up local persistence
+path_to_persistence_dir = os.path.join(
+    os.path.dirname(__file__), os.pardir, "persistence"
+)
+try:
+    os.mkdir(path_to_persistence_dir)
+except OSError:
+    pass
 
 get_context = iteration.get_context
